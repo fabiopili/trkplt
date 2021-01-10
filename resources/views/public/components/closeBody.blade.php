86181734561317e9ffc96cd816ba07f7d13fdc09
Fábio Pili <fabio@pili.com.br>
2021-01-10 15:42:51 -0300

    Added Plausible Analytics.

--- resources/views/public/components/closeBody.blade.php
+++ resources/views/public/components/closeBody.blade.php
@@ -3,3 +3,7 @@
 <div id="svg-icons-container">
 	<?php include(public_path() . '/assets/trkplt/icons/icons.svg'); ?>
 </div>
+
+@if(App::environment('production'))
+	<script async defer data-domain="trkplt.com" src="https://plausible.io/js/plausible.js"></script>
+@endif
